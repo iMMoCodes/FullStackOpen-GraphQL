f5149c454c4c9107214fd0ec90fa1201ca99c4fc
iMMoCodes <matti-makela@hotmail.com>
2022-02-27 22:06:01 +0200

    Tehtävä 8.3

--- index.js
+++ index.js
@@ -101,12 +101,14 @@ const typeDefs = gql`
     name: String!
     id: ID!
     born: Int
+    bookCount: Int
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -115,6 +117,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: ({ name }) =>
+      books.filter((book) => book.author === name).length,
   },
 }
 
